69432b6f4e2beb72324a3388715b9dd57898ff44
ShashankPandey2111 <146189533+ShashankPandey2111@users.noreply.github.com>
2025-12-04 23:44:40 +0530

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,19 @@
+import cv2
+face_cap= cv2.CascadeClassifier("C:/Users/ASUS/AppData/Roaming/Python/Python313/site-packages/cv2/data/haarcascade_frontalface_default.xml")
+video = cv2.VideoCapture(0)
+while True:
+    ret , videodata =video.read()
+    col =cv2.cvtColor(videodata, cv2.COLOR_BGR2GRAY)
+    faces = face_cap.detectMultiScale(
+        col,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30,30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+    for (x,y,w,h) in faces:
+        cv2.rectangle(videodata,(x,y),(x+w,y+h),(0,0,255),2)
+    cv2.imshow("video_live", videodata)
+    if cv2.waitKey()==ord("a"):
+        break
+video.release()
